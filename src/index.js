1cb84ab22fda39cfab317fdbcbf633aaa0801ab9
Shivam Sharma <shivamsharma91197@gmail.com>
2025-08-14 22:21:32 +0530

    add mongodb

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
+import { clearScreenDown } from "readline";
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    console.log(`App listening on port http://localhost:${port}`);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  });
